493aa5440317ab274addd30cc25faf909396c8a8
DHK180100 <titankis2023@gmail.com>
2024-07-08 20:22:32 +0700

    fix login , register , changepassword

--- Infrastructure/obj/Debug/net8.0/Infrastructures.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructures.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructures")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35419d6bad9a3b131ec029fb9f14eef7860c192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6f74c3854bf23966a27af68d23b1893f1bdf38")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructures")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructures")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
